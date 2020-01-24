3f4e6b4d72d2ea84935ee57e6b24c0f5536d92ee
upup2544 <upup2544@hotmail.com>
2020-01-24 15:30:20 +0700

    1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(){	
-
+  float x,y,z,i=1,a,b;
+   cout<<"Enter initial loan: ";
+   cin>>x;
+   cout<<"Enter interest rate per year (%): ";
+   cin>>y;
+   cout<<"Enter amount you can pay per year: ";
+   cin>>z;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,14 +23,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	while(i>0){
+    a=x*y/100;
+	b=x+a;
+	if(b-z<0){
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << x;
+	cout << setw(13) << left << a;
+	cout << setw(13) << left << b;
+	cout << setw(13) << left << b;
+	cout << setw(13) << left << 0;
+	 break;
+	}
+	cout<<fixed<<setprecision(2);
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << x;
+	cout << setw(13) << left << a;
+	cout << setw(13) << left << b;
+	cout << setw(13) << left << z;
+	cout << setw(13) << left << b-z;
+	cout << "\n";
+	 i++;
+	 x=b-z;
+	}	
 	
 	return 0;
 }
